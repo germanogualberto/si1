bf1255c2e30868a1fa2a37c2bea76caca0a3da4e
germanogualberto <gmanopoliano@gmail.com>
2013-11-08 16:29:21 -0300

    Interface mais robusta

--- lab1/src/ditadorDeNumeros/Main.java
+++ lab1/src/ditadorDeNumeros/Main.java
@@ -1,5 +1,6 @@
 package ditadorDeNumeros;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -7,17 +8,38 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 	
-		Scanner ler = new Scanner(System.in);
+		//Scanner ler = new Scanner(System.in);
 		int numero;
 		Ditador d = new Ditador();
+		String opcao;
+		boolean continuar = true;
 		
-		System.out.println("Informe um número: ");
-		numero = ler.nextInt();
-		if (numero<0){
-			System.out.println("Informe um número positivo!");
-		}else{
-			System.out.println( d.numeroPorExtenso(numero) );
+		while(continuar){
+			System.out.println("Informe um número: ");
+			try{
+				Scanner ler = new Scanner(System.in);
+				numero = ler.nextInt();
+				if (numero<0){
+					System.out.println("Informe um número positivo!");
+				}else{
+					System.out.println( d.numeroPorExtenso(numero) );
+				}
+			}catch(InputMismatchException e){
+				System.out.println("Apenas numeros!");
+			}
+			while (true){
+				System.out.println("Ler outro número? s/n");
+				Scanner ler2 = new Scanner(System.in);
+				opcao = ler2.nextLine();
+				if (opcao.equals("n")){
+					continuar = false;
+					break;
+				} else if (opcao.equals("s")){
+					break;
+				}else{
+					System.out.println("Informe uma opção válida!");
+				}
+			}
 		}
-		
 	}
 }
